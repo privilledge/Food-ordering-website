6f60c9fca3334fa0e14dfc6240c31b97e1472387
privilledge <privieroyalty@gmail.com>
2024-10-02 00:04:44 +0200

    home page

---
+++ includes/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>FoodiePal</title>
+    <!-- <link rel="stylesheet" href="./assets/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light text-white mb-5" style="background: transparent;">
+        <div class="container text-white">
+            <a class="navbar-brand fw-bold text-white" href="#">FoodiePal</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse text-white" id="navbar-text">
+                <ul class="navbar-nav mx-auto text-uppercase fw-bold text-white">
+                    <li class="nav-item active  text-white mx-2">
+                        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item  mx-2">
+                        <a class="nav-link" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item  mx-2">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item  mx-2">
+                        <a class="nav-link" href="#">Book table</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                    <button class="btn btn-primary">Order online</button>
+                </span>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
 
 include './includes/config.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>FoodiePal</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets//css/style.css">
+
+</head>
+
+<body>
+    <div class="home">
+        <?php
+        include './includes/header.php';
+        ?>
+        <div class="container col-xxl-8 px-2 py-5 text-white">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5 mt-0 mb-5">
+                <div class="col-10 col-sm-8 col-lg-4">
+                </div>
+                <div class="col-lg-8">
+                    <h1 class="display-5 fw-bold lh-1 mb-5 ">Responsive left-aligned hero with image</h1>
+                    <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                        <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Order now</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
