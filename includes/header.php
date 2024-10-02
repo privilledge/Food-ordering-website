e0a6c64765ac983b8c9e6b7cb2ff52fca9818659
privilledge <privieroyalty@gmail.com>
2024-10-02 22:10:34 +0200

    styling

--- includes/header.php
+++ includes/header.php
@@ -4,11 +4,14 @@
 <head>
     <title>FoodiePal</title>
     <!-- <link rel="stylesheet" href="./assets/css/bootstrap.min.css"> -->
-    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <link rel="stylesheet" href="../assets/css/style.css">
     <!-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Qwitcher+Grypen:wght@400;700&display=swap" rel="stylesheet">
 
 
 </head>
@@ -17,15 +20,15 @@
 
     <nav class="navbar navbar-expand-lg navbar-light text-white mb-5" style="background: transparent;">
         <div class="container text-white">
-            <a class="navbar-brand fw-bold text-white" href="#">FoodiePal</a>
+            <a class="navbar-brand fw-bold text-white fs-3" href="#">FoodiePal</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse text-white" id="navbar-text">
                 <ul class="navbar-nav mx-auto text-uppercase fw-bold text-white">
-                    <li class="nav-item active  text-white mx-2">
-                        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+                    <li class="nav-item active   mx-2">
+                        <a class="nav-link text-white" href="#">Home <span class="sr-only"></span></a>
                     </li>
                     <li class="nav-item  mx-2">
                         <a class="nav-link" href="#">Menu</a>
@@ -38,6 +41,15 @@
                     </li>
                 </ul>
                 <span class="navbar-text">
+                    <span><svg stroke="#fff" fill="#fff" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path>
+                        </svg></span>
+                    <span><svg stroke="#fff" fill="#fff" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M21.822,7.431C21.635,7.161,21.328,7,21,7H7.333L6.179,4.23C5.867,3.482,5.143,3,4.333,3H2v2h2.333l4.744,11.385 C9.232,16.757,9.596,17,10,17h8c0.417,0,0.79-0.259,0.937-0.648l3-8C22.052,8.044,22.009,7.7,21.822,7.431z M17.307,15h-6.64 l-2.5-6h11.39L17.307,15z"></path>
+                            <circle cx="10.5" cy="19.5" r="1.5"></circle>
+                            <circle cx="17.5" cy="19.5" r="1.5"></circle>
+                        </svg></span>
+
                     <button class="btn btn-primary">Order online</button>
                 </span>
             </div>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include './includes/config.php';
 <head>
     <title>FoodiePal</title>
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets//css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 
 </head>
 
@@ -18,16 +18,29 @@ include './includes/config.php';
         <?php
         include './includes/header.php';
         ?>
-        <div class="container col-xxl-8 px-2 py-5 text-white">
-            <div class="row flex-lg-row-reverse align-items-center g-5 py-5 mt-0 mb-5">
-                <div class="col-10 col-sm-8 col-lg-4">
+        <div class="container col-xxl-8 text-white " style="height: 600px;">
+            <div class="row flex-lg-row-reverse align-items-center g-5">
+                <div class="col-10 col-sm-8 col-lg-6">
                 </div>
-                <div class="col-lg-8">
-                    <h1 class="display-5 fw-bold lh-1 mb-5 ">Responsive left-aligned hero with image</h1>
-                    <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <div class="col-lg-6">
+                    <h1 class="display-5 fw-bold lh-1 mb-3 mt-5">FoodiePal Restaurant</h1>
+                    <p class="">Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad mollitia laborum quam quisquam esse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.</p>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-start">
                         <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Order now</button>
-
+                    </div>
+                    <div class="icons d-flex mt-5 py-5">
+                        <svg stroke="#fff" fill="#fff" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3 2H5V22H3zM10 6H17V8H10zM10 10H17V12H10z"></path>
+                            <path d="M19,2h-5h-0.063H6v20h7.938H14h5c1.103,0,2-0.897,2-2V4C21,2.897,20.103,2,19,2z M19,20h-5h-0.063H8V4h5.938H14h5V20z"></path>
+                        </svg>
+                        <svg stroke="#fff" fill="#fff" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M21.822,7.431C21.635,7.161,21.328,7,21,7H7.333L6.179,4.23C5.867,3.482,5.143,3,4.333,3H2v2h2.333l4.744,11.385 C9.232,16.757,9.596,17,10,17h8c0.417,0,0.79-0.259,0.937-0.648l3-8C22.052,8.044,22.009,7.7,21.822,7.431z M17.307,15h-6.64 l-2.5-6h11.39L17.307,15z"></path>
+                            <circle cx="10.5" cy="19.5" r="1.5"></circle>
+                            <circle cx="17.5" cy="19.5" r="1.5"></circle>
+                        </svg>
+                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17.707,12.293c-0.391-0.391-1.023-0.391-1.414,0l-1.594,1.594c-0.739-0.22-2.118-0.72-2.992-1.594 s-1.374-2.253-1.594-2.992l1.594-1.594c0.391-0.391,0.391-1.023,0-1.414l-4-4c-0.391-0.391-1.023-0.391-1.414,0L3.581,5.005 c-0.38,0.38-0.594,0.902-0.586,1.435c0.023,1.424,0.4,6.37,4.298,10.268s8.844,4.274,10.269,4.298c0.005,0,0.023,0,0.028,0 c0.528,0,1.027-0.208,1.405-0.586l2.712-2.712c0.391-0.391,0.391-1.023,0-1.414L17.707,12.293z M17.58,19.005 c-1.248-0.021-5.518-0.356-8.873-3.712c-3.366-3.366-3.692-7.651-3.712-8.874L7,4.414L9.586,7L8.293,8.293 C8.054,8.531,7.952,8.875,8.021,9.205c0.024,0.115,0.611,2.842,2.271,4.502s4.387,2.247,4.502,2.271 c0.333,0.071,0.674-0.032,0.912-0.271L17,14.414L19.586,17L17.58,19.005z"></path>
+                        </svg>
                     </div>
                 </div>
             </div>
